f1f2176225bb14987373e45b817db4559150cf98
Karol Lozano González <karolanette@Karols-MacBook-Pro-3.local>
2025-10-06 11:21:56 -0600

    Archivos txt + corrección sais.py

---
+++ sais.py
@@ -0,0 +1,126 @@
+import time
+import sys
+
+def getBuckets(T):
+    count = {}
+    buckets = {}
+    for c in T:
+        count[c] = count.get(c, 0) + 1
+    start = 0
+    for c in sorted(count.keys()):
+        buckets[c] = (start, start + count[c])
+        start += count[c]
+    return buckets
+
+def sais(T):
+    t = ["_"] * len(T)
+    
+    t[len(T) - 1] = "S"
+    for i in range(len(T) - 1, 0, -1):
+        if T[i-1] == T[i]:
+            t[i - 1] = t[i]
+        else:
+            t[i - 1] = "S" if T[i-1] < T[i] else "L"
+    
+    buckets = getBuckets(T)
+
+    count = {}
+    SA = [-1] * len(T)
+    LMS = {}
+    end = None
+    for i in range(len(T) - 1, 0, -1):
+        if t[i] == "S" and t[i - 1] == "L":
+            revoffset = count[T[i]] = count.get(T[i], 0) + 1
+            SA[buckets[T[i]][1] - revoffset] = i
+            if end is not None:
+                LMS[i] = end
+            end = i
+
+    LMS[len(T) - 1] = len(T) - 1
+    count = {}
+    for i in range(len(T)):
+        if SA[i] >= 0:
+            if t[SA[i] - 1] == "L":
+                symbol = T[SA[i] - 1]
+                offset = count.get(symbol, 0)
+                SA[buckets[symbol][0] + offset] = SA[i] - 1
+                count[symbol] = offset + 1
+
+    count = {}
+    for i in range(len(T) - 1, 0, -1):
+        if SA[i] > 0:
+            if t[SA[i] - 1] == "S":
+                symbol = T[SA[i] - 1]
+                revoffset = count[symbol] = count.get(symbol, 0) + 1
+                SA[buckets[symbol][1] - revoffset] = SA[i] - 1
+
+
+    namesp = [-1] * len(T)
+    name = 0
+    prev = None
+    for i in range(len(SA)):
+        if t[SA[i]] == "S" and t[SA[i] - 1] == "L":
+            end_prev = LMS.get(SA[prev], len(T)) if prev is not None else None
+            end_i = LMS.get(SA[i], len(T))
+            if prev is not None and T[SA[prev]:end_prev] != T[SA[i]:end_i]:
+                name += 1
+            prev = i
+            namesp[SA[i]] = name
+
+    names = []
+    SApIdx = []
+    for i in range(len(T)):
+        if namesp[i] != -1:
+            names.append(namesp[i])
+            SApIdx.append(i)
+
+    if name < len(names) - 1:
+        names = sais(names)
+
+    names = list(reversed(names))
+
+    SA = [-1] * len(T)
+    count = {}
+    for i in range(len(names)):
+        pos = SApIdx[names[i]]
+        revoffset = count[T[pos]] = count.get(T[pos], 0) + 1
+        SA[buckets[T[pos]][1] - revoffset] = pos
+
+    count = {}
+    for i in range(len(T)):
+        if SA[i] >= 0:
+            if t[SA[i] - 1] == "L":
+                symbol = T[SA[i] - 1]
+                offset = count.get(symbol, 0)
+                SA[buckets[symbol][0] + offset] = SA[i] - 1
+                count[symbol] = offset + 1
+
+    count = {}
+    for i in range(len(T) - 1, 0, -1):
+        if SA[i] > 0:
+            if t[SA[i] - 1] == "S":
+                symbol = T[SA[i] - 1]
+                revoffset = count[symbol] = count.get(symbol, 0) + 1
+                SA[buckets[symbol][1] - revoffset] = SA[i] - 1
+
+    return SA
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Elige un archivo")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+
+    with open(filename, "r", encoding="utf-8") as f:
+        text = f.read().strip() + "$" 
+
+    T = [ord(c) for c in text]
+
+    start = time.time()
+    SA = sais(T)
+    end = time.time()
+
+    print(f"Tiempo de ejecución: {end - start:.6f} segundos.")
+    print(f"Tamaño del archivo: {len(T)} caracteres")
+    print("Primeros 100 índices:", SA[:100])

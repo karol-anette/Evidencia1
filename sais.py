91733640e8fd6d5be49245198e87cad9743869e2
Karol Lozano González <karolanette@Karols-MacBook-Pro-3.local>
2025-10-06 19:10:56 -0600

    Cambio a por referencia en sais.py

--- sais.py
+++ sais.py
@@ -13,21 +13,8 @@ def getBuckets(T):
         start += count[c]
     return buckets
 
-def sais(T):
-    t = ["_"] * len(T)
-    
-    t[len(T) - 1] = "S"
-    for i in range(len(T) - 1, 0, -1):
-        if T[i-1] == T[i]:
-            t[i - 1] = t[i]
-        else:
-            t[i - 1] = "S" if T[i-1] < T[i] else "L"
-    
-    buckets = getBuckets(T)
 
-    count = {}
-    SA = [-1] * len(T)
-    LMS = {}
+def place_LMS(SA, LMS, count, buckets, T, t):
     end = None
     for i in range(len(T) - 1, 0, -1):
         if t[i] == "S" and t[i - 1] == "L":
@@ -36,37 +23,61 @@ def sais(T):
             if end is not None:
                 LMS[i] = end
             end = i
-
     LMS[len(T) - 1] = len(T) - 1
-    count = {}
-    for i in range(len(T)):
-        if SA[i] >= 0:
-            if t[SA[i] - 1] == "L":
-                symbol = T[SA[i] - 1]
-                offset = count.get(symbol, 0)
-                SA[buckets[symbol][0] + offset] = SA[i] - 1
-                count[symbol] = offset + 1
 
-    count = {}
+def induce_L(SA, count, buckets, T, t):
+    for i in range(len(SA)):
+        if SA[i] >= 0 and t[SA[i] - 1] == "L":
+            symbol = T[SA[i] - 1]
+            offset = count.get(symbol, 0)
+            SA[buckets[symbol][0] + offset] = SA[i] - 1
+            count[symbol] = offset + 1
+
+def induce_S(SA, count, buckets, T, t):
+    for i in range(len(SA) - 1, 0, -1):
+        if SA[i] > 0 and t[SA[i] - 1] == "S":
+            symbol = T[SA[i] - 1]
+            revoffset = count[symbol] = count.get(symbol, 0) + 1
+            SA[buckets[symbol][1] - revoffset] = SA[i] - 1
+
+def sais(T):
+    t = ["_"] * len(T)
+
+    t[- 1] = "S"
     for i in range(len(T) - 1, 0, -1):
-        if SA[i] > 0:
-            if t[SA[i] - 1] == "S":
-                symbol = T[SA[i] - 1]
-                revoffset = count[symbol] = count.get(symbol, 0) + 1
-                SA[buckets[symbol][1] - revoffset] = SA[i] - 1
+        if T[i-1] < T[i]:
+            t[i - 1] = "S"
+        elif T[i - 1] == T[i] and t[i] == "S":
+            t[i - 1] = "S"
+        else:
+            t[i - 1] = "L"
+    
+    buckets = getBuckets(T)
 
+    count = {}
+    SA = [-1] * len(T)
+    LMS = {}
+  
+    place_LMS(SA, LMS, count, buckets, T, t)
 
+    count.clear()
+    induce_L(SA, count, buckets, T, t)
+
+    count.clear()
+    induce_S(SA, count, buckets, T, t)
+ 
     namesp = [-1] * len(T)
     name = 0
     prev = None
-    for i in range(len(SA)):
-        if t[SA[i]] == "S" and t[SA[i] - 1] == "L":
-            end_prev = LMS.get(SA[prev], len(T)) if prev is not None else None
-            end_i = LMS.get(SA[i], len(T))
-            if prev is not None and T[SA[prev]:end_prev] != T[SA[i]:end_i]:
+    LMS_indices = [i for i in range(len(T)) if t[i] == "S" and t[i - 1] == "L"]
+    for idx, i in enumerate(LMS_indices):
+        end_i = LMS.get(i, len(T))
+        if prev is not None:
+            end_prev = LMS.get(LMS_indices[prev], len(T))
+            if T[LMS_indices[prev]:end_prev] != T[i:end_i]:
                 name += 1
-            prev = i
-            namesp[SA[i]] = name
+        namesp[i] = name
+        prev = idx
 
     names = []
     SApIdx = []
@@ -81,28 +92,16 @@ def sais(T):
     names = list(reversed(names))
 
     SA = [-1] * len(T)
-    count = {}
-    for i in range(len(names)):
-        pos = SApIdx[names[i]]
+    count.clear()
+    for i, pos in enumerate(SApIdx):
+        idx = names[i]
         revoffset = count[T[pos]] = count.get(T[pos], 0) + 1
         SA[buckets[T[pos]][1] - revoffset] = pos
 
-    count = {}
-    for i in range(len(T)):
-        if SA[i] >= 0:
-            if t[SA[i] - 1] == "L":
-                symbol = T[SA[i] - 1]
-                offset = count.get(symbol, 0)
-                SA[buckets[symbol][0] + offset] = SA[i] - 1
-                count[symbol] = offset + 1
-
-    count = {}
-    for i in range(len(T) - 1, 0, -1):
-        if SA[i] > 0:
-            if t[SA[i] - 1] == "S":
-                symbol = T[SA[i] - 1]
-                revoffset = count[symbol] = count.get(symbol, 0) + 1
-                SA[buckets[symbol][1] - revoffset] = SA[i] - 1
+    count.clear()
+    induce_L(SA, count, buckets, T, t)
+    count.clear()
+    induce_S(SA, count, buckets, T, t)
 
     return SA
 
